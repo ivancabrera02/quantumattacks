afc1c918e921dfa75f273a3be18959d1b9b2236b
Iván Cabrera <103500562+ivancabrera02@users.noreply.github.com>
2024-03-04 17:07:30 +0100

    Add files via upload

---
+++ QTM/main.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    var nav = document.getElementById('myNav');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+}
